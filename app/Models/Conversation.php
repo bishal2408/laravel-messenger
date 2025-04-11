05631c32886f38142e10939f1a40de43f2270408
BISHAL LAMICHHANE <bishal.lamichhane@diagonal.software>
2025-04-11 23:32:31 +0545

    feat: add models, and migrations

---
+++ app/Models/Conversation.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Conversation extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'user_id2',
+        'last_message_id',
+    ];
+}
---
+++ app/Models/Message.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'message',
+        'sender_id',
+        'group_id',
+        'receiver_id',
+    ];
+}
