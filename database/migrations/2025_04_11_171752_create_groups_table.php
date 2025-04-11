23bc9a637a42851c27f4c1a4c848a5eec8ad923c
BISHAL LAMICHHANE <bishal.lamichhane@diagonal.software>
2025-04-12 00:41:38 +0545

    fix: migration issue, feat: add factory and seeder

--- database/migrations/2025_04_11_171752_create_groups_table.php
+++ database/migrations/2025_04_11_171752_create_groups_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::table('group_users', function (Blueprint $table) {
+        Schema::create('group_users', function (Blueprint $table) {
             $table->foreignId('group_id')->constrained('groups')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
