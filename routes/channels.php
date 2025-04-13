0242260d60e6600566c152a791ea786e46955c7c
BISHAL LAMICHHANE <bishal.lamichhane@diagonal.software>
2025-04-13 19:48:31 +0545

    feat: add endpoint to get message by conversation, groop, load old message, store, and delete message. feat: add broadcasting for group and normal message

--- routes/channels.php
+++ routes/channels.php
@@ -1,8 +1,29 @@
 <?php
 
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('online', function ($user) {
+// Chennel to get online user and its resources
+Broadcast::channel('online', function (User $user) {
     return $user ? new UserResource($user) : null;
 });
+
+// Channel for one-to-one communication
+// The closure ensures that one channel is not duplicated twice
+Broadcast::channel(
+    'message.user.{userId1}-{userId2}',
+    function (User $user, int $userId1, int $userId2) {
+        // validating if current user has access to the channel
+        return $user->id === $userId1 || $user->id === $userId2 ? $user : null;
+    }
+);
+
+// Channel for group messages
+Broadcast::channel(
+    'message.group.{groupId}',
+    function (User $user, int $groupId) {
+        // validating if current user has access to the group
+        return $user->groups->contains('id', $groupId) ? $user : null;
+    }
+);
