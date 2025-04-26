210b34fcc8f33b5078b4b92d355952e679e84b4d
BISHAL LAMICHHANE <bishal.lamichhane@diagonal.software>
2025-04-26 12:51:59 +0545

    feat: add message input field

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import ConversationHeader from "@/Components/App/ConversationHeader";
+import MessageInput from "@/Components/App/MessageInput";
 import MessageItem from "@/Components/App/MessageItem";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import ChatLayout from "@/Layouts/ChatLayout";
@@ -62,7 +63,7 @@ function Home({ selectedConversation = null, messages = null }) {
                         )}
                     </div>
 
-                    {/* <MessageInput conversation={selectedConversation} /> */}
+                    <MessageInput conversation={selectedConversation} />
                 </>
             )}
         </>
